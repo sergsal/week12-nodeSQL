b25ba05ad904283f8f4ebbb3d50c3d003e20356b
Sergio Saldarriaga <sergio.saldarriaga88@gmail.com>
2016-09-15 14:55:54 -0400

    completes customer code

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -11,10 +11,25 @@ var connection = mysql.createConnection({
 });
 //creating table object to display products to customer
 var table = new Table({
-    head: ['Item ID', 'Product', 'Price']
-  , colWidths: [20, 20, 10]
+ head: ['Item ID', 'Product', 'Price'],
+ colWidths: [20, 20, 10]
 });
-
+var schema = {
+ properties: {
+  productid: {
+   pattern: /^(0|[1-9][0-9]*)$/,
+   message: 'Product ID can be only numbers',
+   required: true,
+   description: "Please enter the ID of the product you would like to purchase"
+  },
+  quantity: {
+   pattern: /^(0|[1-9][0-9]*)$/,
+   message: 'Quantity can be only numbers',
+   required: true,
+   description: "Please enter how many you would like to buy"
+  }
+ }
+};
 
 connection.connect(function (err) {
  if (err) throw err;
@@ -22,21 +37,40 @@ connection.connect(function (err) {
 
 connection.query('SELECT ItemID,ProductName,Price FROM products', function (err, res) {
  if (err) throw err;
- res.forEach(function(value){
+ res.forEach(function (value) {
   table.push(
     [value.ItemID, value.ProductName, value.Price]
-);
+  );
  });
- console.log(table.toString());
+ console.log("\n" +table.toString());
 });
-connection.end(function (err) {
- if (err) throw err;
+//connection.query('SELECT Price FROM products WHERE ItemID = ?',[4], function(err, res) {
+//  console.log(res[0].Price);
+// })
+
+prompt.get(schema, function (err, result) {
+ var prodId = result.productid;
+ var custQuant = result.quantity;
+ connection.query('SELECT Price,StockQuantity FROM products WHERE ItemID = ?', [prodId], function (err, res) {
+  var prodPrice = res[0].Price;
+  var availQuant = res[0].StockQuantity;
+  var totalCost = (prodPrice * custQuant);
+  var newStock = (availQuant - custQuant);
+  if ((newStock) < 0) {
+   console.log("Insufficient availability!");
+   connection.end();
+  } else if (newStock >= 0){
+   connection.query('UPDATE products SET ? WHERE ?', [{StockQuantity: newStock},
+     {ItemID: prodId}], function(err, res) {
+    if (err) throw err;
+    console.log("Your total cost is $" + totalCost.toFixed(2));
+    connection.end()
+   });
+  };
+ });
+ //console.log(result.id)
 });
-
-prompt.start();
-
-prompt.get(['id', 'quantity'], function (err, result) {
- console.log(" Product ID: " + result.id);
- console.log(" Quanitity: " + result.quantity);
-})
-
+//connection.end(function (err) {
+// if (err) throw err;
+//});
+ prompt.start();
